560ad5978019b904d8b9c8c51707986688ec5ece
aigerimmazhikenova <96471339+aigerimmazhikenova@users.noreply.github.com>
2023-01-04 22:34:41 +0600

    Add files via upload

--- greenlight/cmd/api/errors.go
+++ greenlight/cmd/api/errors.go
@@ -8,7 +8,6 @@ import (
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Print(err)
 }
-
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 	env := envelope{"error": message}
 	err := app.writeJSON(w, status, env, nil)
@@ -16,24 +15,20 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 		app.logError(r, err)
 		w.WriteHeader(500)
 	}
-
 }
-
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
-
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
-	message := "the requested resource couldn't not be found"
+	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
-
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
 
@@ -43,4 +38,9 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
 }
--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"context"
+	"context" // New import
 	"flag"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"greenlight.nurym.net/internal/data"
 	"log"
 	"net/http"
 	"os"
@@ -27,73 +28,56 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
 	var cfg config
-
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	print("HERE")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
-
 	flag.Parse()
-	print(cfg.db.dsn)
-
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	print("sadsadasd")
 	db, err := openDB(cfg)
 	if err != nil {
 		logger.Fatal(err)
 	}
-
 	defer db.Close()
-
 	logger.Printf("database connection pool established")
-
+	// Use the data.NewModels() function to initialize a Models struct, passing in the
+	// connection pool as a parameter.
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	srv := &http.Server{
-		Addr:              fmt.Sprintf(":%d", cfg.port),
-		Handler:           app.routes(),
-		IdleTimeout:       time.Minute,
-		ReadHeaderTimeout: 10 * time.Second,
-		WriteTimeout:      30 * time.Second,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err = srv.ListenAndServe()
 	logger.Fatal(err)
-
 }
 
 func openDB(cfg config) (*pgxpool.Pool, error) {
+	var err error
 	db, err := pgxpool.New(context.Background(), cfg.db.dsn)
 	if err != nil {
-		return nil, err
+		fmt.Fprintln(os.Stderr, "Unable to connect to database:", err)
+		os.Exit(1)
 	}
-	// Set the maximum number of open (in-use + idle) connections in the pool. Note that
-	// passing a value less than or equal to 0 will mean there is no limit.
 	db.Config().MaxConns = int32(cfg.db.maxOpenConns)
-	// Set the maximum number of idle connections in the pool. Again, passing a value
-	// less than or equal to 0 will mean there is no limit.
-
-	// Use the time.ParseDuration() function to convert the idle timeout duration string
-	// to a time.Duration type.
 	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
 	if err != nil {
 		return nil, err
 	}
-	// Set the maximum idle timeout.
 	db.Config().MaxConnIdleTime = duration
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -101,7 +85,6 @@ func openDB(cfg config) (*pgxpool.Pool, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	fmt.Println("Success connection")
 
 	return db, nil
